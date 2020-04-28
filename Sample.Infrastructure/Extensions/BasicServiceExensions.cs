151bfe6ab6735b28bb02fd16dcbf52b60b69e532
hanj <http://git.dev.ft>
2020-04-28 18:35:14 +0800

    Add Basicservice

---
+++ Sample.Infrastructure/Extensions/BasicServiceExensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Infrastructure.Interfaces.Extensions
+{
+   public static  class BasicServiceExensions
+    {
+        public static IServiceCollection AddBasicService(this IServiceCollection services) {
+
+         var configuration= services.BuildServiceProvider().GetService<IConfiguration>();        
+        }
+    }
+}
